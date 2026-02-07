27c75f162bd021e3e011f5dd7f1fe503a5f6d635
mayandey-947 <mayandey947@gmail.com>
2026-02-07 14:21:44 +0530

    cHANGES FROM gb...

---
+++ includes/sections/breadcrumb-section.php
@@ -0,0 +1,109 @@
+<!-- Breadcrumb section Start-->
+<div class="breadcrumb-section two">
+    <div class="swiper home2-banner-slider">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">
+                <div class="banner-bg"
+                    style="background-image:linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(<?= BASE_URL ?>/assets/img/innerpages/breadcrumb-bg3.jpg);">
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <div class="banner-bg"
+                    style="background-image:linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(<?= BASE_URL ?>/assets/img/innerpages/breadcrumb-bg4.jpg);">
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <div class="banner-bg"
+                    style="background-image:linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(<?= BASE_URL ?>/assets/img/innerpages/experience-breadcrumb-bg.jpg);">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="banner-content-wrap">
+        <div class="container">
+            <div class="banner-content">
+                <span>Starting From <strong>$99</strong>/per person</span>
+                <h1>Zip-lining & Canopy</h1>
+                <div class="batch">
+                    <span>01 Hour | Himachal Pradesh, India</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="slider-btn-grp">
+        <div class="slider-btn banner-slider-prev">
+            <svg width="22" height="22" viewBox="0 0 22 22" xmlns="http://www.w3.org/2000/svg">
+                <g>
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M0 10.0571H22V11.9428H0V10.0571Z" />
+                    <path fill-rule="evenodd" clip-rule="evenodd"
+                        d="M0.942857 11.9429C5.3768 11.9429 9.00115 8.0432 9.00115 3.88457V2.94171H7.11543V3.88457C7.11543 7.04251 4.29566 10.0571 0.942857 10.0571H0V11.9429H0.942857Z" />
+                    <path fill-rule="evenodd" clip-rule="evenodd"
+                        d="M0.942857 10.0571C5.3768 10.0571 9.00115 13.9568 9.00115 18.1154V19.0583H7.11543V18.1154C7.11543 14.9587 4.29566 11.9428 0.942857 11.9428H0V10.0571H0.942857Z" />
+                </g>
+            </svg>
+        </div>
+        <div class="slider-btn banner-slider-next">
+            <svg width="22" height="22" viewBox="0 0 22 22" xmlns="http://www.w3.org/2000/svg">
+                <g>
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M22 10.0571H-5.72205e-06V11.9428H22V10.0571Z" />
+                    <path fill-rule="evenodd" clip-rule="evenodd"
+                        d="M21.0571 11.9429C16.6232 11.9429 12.9989 8.0432 12.9989 3.88457V2.94171H14.8846V3.88457C14.8846 7.04251 17.7043 10.0571 21.0571 10.0571H22V11.9429H21.0571Z" />
+                    <path fill-rule="evenodd" clip-rule="evenodd"
+                        d="M21.0571 10.0571C16.6232 10.0571 12.9989 13.9568 12.9989 18.1154V19.0583H14.8846V18.1154C14.8846 14.9587 17.7043 11.9428 21.0571 11.9428H22V10.0571H21.0571Z" />
+                </g>
+            </svg>
+        </div>
+    </div>
+</div>
+<div class="package-details-breadcrumb-bottom">
+    <div class="container">
+        <div class="details-breadcrumb-bottom-wrapper">
+            <div class="left-content">
+                <ul>
+                    <li>
+                        <svg width="12" height="12" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M11.544 3.68665L4.87161 10.3626C4.76529 10.4653 4.63053 10.5186 4.49589 10.5186C4.42605 10.5187 4.35687 10.505 4.29236 10.4782C4.22785 10.4514 4.16929 10.4122 4.12005 10.3626L0.457651 6.70021C0.248491 6.49453 0.248491 6.15781 0.457651 5.94865L1.75173 4.65097C1.95033 4.45249 2.30481 4.45249 2.50341 4.65097L4.49589 6.64346L9.49833 1.63741C9.54761 1.58803 9.60613 1.54883 9.67055 1.52206C9.73498 1.49529 9.80405 1.48148 9.87381 1.48141C10.0155 1.48141 10.1503 1.53817 10.2495 1.63741L11.5436 2.93521C11.7531 3.14437 11.7531 3.48109 11.544 3.68665Z" />
+                        </svg>
+                        No Booking Fee
+                    </li>
+                    <li>
+                        <svg width="12" height="12" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M11.544 3.68665L4.87161 10.3626C4.76529 10.4653 4.63053 10.5186 4.49589 10.5186C4.42605 10.5187 4.35687 10.505 4.29236 10.4782C4.22785 10.4514 4.16929 10.4122 4.12005 10.3626L0.457651 6.70021C0.248491 6.49453 0.248491 6.15781 0.457651 5.94865L1.75173 4.65097C1.95033 4.45249 2.30481 4.45249 2.50341 4.65097L4.49589 6.64346L9.49833 1.63741C9.54761 1.58803 9.60613 1.54883 9.67055 1.52206C9.73498 1.49529 9.80405 1.48148 9.87381 1.48141C10.0155 1.48141 10.1503 1.53817 10.2495 1.63741L11.5436 2.93521C11.7531 3.14437 11.7531 3.48109 11.544 3.68665Z" />
+                        </svg>
+                        Best Price Ever
+                    </li>
+                </ul>
+                <a href="https://www.tripadvisor.com/" class="rating-area">
+                    <img src="assets/img/innerpages/icon/tripadvisor-rating.svg" alt="">
+                    <strong>(4.5/5)</strong>
+                    <span>based on 138 reviews</span>
+                </a>
+            </div>
+            <div class="right-content">
+                <span>
+                    <img src="assets/img/innerpages/icon/carbon-icon.svg" alt="">
+                    100% Carbon Neutral
+                </span>
+                <div class="share-btn">
+                    <div class="share-icon">
+                        <svg width="14" height="14" viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg">
+                            <g>
+                                <path
+                                    d="M11.1016 9.07812C10.2911 9.07812 9.57685 9.47682 9.12825 10.0831L5.24973 8.09714C5.31412 7.87765 5.35938 7.65018 5.35938 7.41016C5.35938 7.0846 5.29263 6.7751 5.17732 6.49042L9.23636 4.04786C9.68808 4.578 10.352 4.92188 11.1016 4.92188C12.4586 4.92188 13.5625 3.81798 13.5625 2.46094C13.5625 1.10389 12.4586 0 11.1016 0C9.74452 0 8.64062 1.10389 8.64062 2.46094C8.64062 2.77367 8.70502 3.07032 8.81185 3.34573L4.74072 5.79545C4.28939 5.28106 3.635 4.94922 2.89844 4.94922C1.54139 4.94922 0.4375 6.05311 0.4375 7.41016C0.4375 8.7672 1.54139 9.87109 2.89844 9.87109C3.7223 9.87109 4.44858 9.46069 4.89549 8.83734L8.76124 10.8169C8.69004 11.0467 8.64062 11.2861 8.64062 11.5391C8.64062 12.8961 9.74452 14 11.1016 14C12.4586 14 13.5625 12.8961 13.5625 11.5391C13.5625 10.182 12.4586 9.07812 11.1016 9.07812Z" />
+                            </g>
+                        </svg>
+                    </div>
+                    <ul class="social-list">
+                        <li><a href="https://www.facebook.com/"><i class="bx bxl-facebook"></i></a></li>
+                        <li><a href="https://www.linkedin.com/"><i class="bx bxl-linkedin"></i></a></li>
+                        <li><a href="https://www.youtube.com/"><i class="bx bxl-youtube"></i></a></li>
+                        <li><a href="https://www.instagram.com/"><i class="bx bxl-instagram-alt"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Breadcrumb section End-->
