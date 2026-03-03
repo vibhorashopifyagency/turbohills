f2d8e2c828042b7db239e8613b86a5675f08f109
mayandey-947 <mayandey947@gmail.com>
2026-03-03 19:49:17 +0530

    updateing the home page

---
+++ includes/sections/home-3/guides.php
@@ -0,0 +1,80 @@
+<div class="home4-destination-section mb-100">
+        <div class="container">
+            <div class="row justify-content-center mb-50 wow animate fadeInDown" data-wow-delay="200ms" data-wow-duration="1500ms">
+                <div class="col-xl-6 col-lg-8">
+                    <div class="section-title text-center">
+                        <h2>Our Local Experts Guides</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="destination-slider-area mb-40">
+                <div class="swiper home3-destination-slider">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide">
+                            <div class="destination-card2 three">
+                                <div class="destination-img">
+                                    <img src="<?= BASE_URL ?>/assets/img/guides/guide1.jpg" alt="">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="destination-card2 three">
+                                <div class="destination-img">
+                                    <img src="<?= BASE_URL ?>/assets/img/guides/guide2.jpg" alt="">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="destination-card2 three">
+                                <div class="destination-img">
+                                    <img src="<?= BASE_URL ?>/assets/img/guides/guide3.jpg" alt="">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="destination-card2 three">
+                                <div class="destination-img">
+                                    <img src="<?= BASE_URL ?>/assets/img/guides/guide4.jpg" alt="">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="swiper-slide">
+                            <div class="destination-card2 three">
+                                <div class="destination-img">
+                                    <img src="<?= BASE_URL ?>/assets/img/guides/guide5.jpg" alt="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="slider-btn-grp two">
+                    <div class="slider-btn destination-slider-prev">
+                        <svg width="16" height="12" viewBox="0 0 16 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M0 5.31421H16V6.68564H0V5.31421Z"/>
+                            <path fill-rule="evenodd" clip-rule="evenodd"
+                                d="M0.685714 6.68569C3.9104 6.68569 6.54629 3.84958 6.54629 0.825119V0.139404H5.17486V0.825119C5.17486 3.12181 3.12412 5.31426 0.685714 5.31426H0V6.68569H0.685714Z"/>
+                            <path fill-rule="evenodd" clip-rule="evenodd"
+                                d="M0.685714 5.31421C3.9104 5.31421 6.54629 8.15032 6.54629 11.1748V11.8605H5.17486V11.1748C5.17486 8.87901 3.12412 6.68564 0.685714 6.68564H0V5.31421H0.685714Z"/>
+                        </svg>
+                    </div>
+                    <div class="slider-btn destination-slider-next">
+                        <svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+                            <g>
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M16 7.31421H-3.8147e-06V8.68564H16V7.31421Z"/>
+                                <path fill-rule="evenodd" clip-rule="evenodd"
+                                    d="M15.3143 8.68569C12.0896 8.68569 9.45371 5.84958 9.45371 2.82512V2.1394H10.8251V2.82512C10.8251 5.12181 12.8759 7.31426 15.3143 7.31426H16V8.68569H15.3143Z"/>
+                                <path fill-rule="evenodd" clip-rule="evenodd"
+                                    d="M15.3143 7.31421C12.0896 7.31421 9.45371 10.1503 9.45371 13.1748V13.8605H10.8251V13.1748C10.8251 10.879 12.8759 8.68564 15.3143 8.68564H16V7.31421H15.3143Z"/>
+                            </g>
+                        </svg>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12 d-flex justify-content-center">
+                    <div class="swiper-pagination1 paginations two"></div>
+                </div>
+            </div>
+        </div>
+        <img src="<?= BASE_URL ?>/assets/img/home4/vector/destination-section-vector.svg" alt="" class="vector">
+    </div>
--- index.php
+++ index.php
@@ -106,8 +106,10 @@
     include __DIR__ . '/includes/sections/home-3/home3-about-section.php'; // Left Image with text
     include __DIR__ . '/includes/sections/home2-parallax-section.php'; // Parallax section
     include __DIR__ . '/includes/sections/home2-destination-section.php'; // Destination Slider
-    include __DIR__ . '/includes/sections/home2-service-section.php'; // Sevice center banner
-    include __DIR__ . '/includes/sections/home-3/home3-tour-guide.php'; // Tour Guide Slider
+    // include __DIR__ . '/includes/sections/home2-service-section.php'; // Sevice center banner
+    include __DIR__ . '/includes/sections/cards.php'; // Sevice center banner
+    // include __DIR__ . '/includes/sections/home-3/home3-tour-guide.php'; // Tour Guide Slider
+    include __DIR__ . '/includes/sections/home-3/guides.php'; // Tour Guide Slider
     // include __DIR__ . '/includes/sections/home-5/home5-about-section.php'; // Tour Guide Slider
     // include __DIR__ . '/includes/sections/home-6/home6-service-section.php'; // Purple service section
     // include __DIR__ . '/includes/sections/home2-about-us.php'; // Trustpoint section
